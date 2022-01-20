18f43c2d4c5d197ab3b669588f6adf9df282fdec
yoshiu0528 <yoshiyuki.u528@gmail.com>
2022-01-21 00:38:21 +0900

    Update wage_streamlit.py

--- wage_streamlit.py
+++ wage_streamlit.py
@@ -6,9 +6,9 @@ import plotly.express as px
 
 st.title('日本の賃金データダッシュボード')
 
-df_jp_ind = pd.read_csv('./雇用_医療福祉_一人当たり賃金_全国_全産業.csv',encoding='shift_jis')
-df_jp_category = pd.read_csv('./雇用_医療福祉_一人当たり賃金_全国_大分類.csv',encoding='shift_jis')
-df_pref_ind = pd.read_csv('./雇用_医療福祉_一人当たり賃金_都道府県_全産業.csv',encoding='shift_jis')
+df_jp_ind = pd.read_csv('./csv_data/雇用_医療福祉_一人当たり賃金_全国_全産業.csv',encoding='shift_jis')
+df_jp_category = pd.read_csv('./csv_data/雇用_医療福祉_一人当たり賃金_全国_大分類.csv',encoding='shift_jis')
+df_pref_ind = pd.read_csv('./csv_data/雇用_医療福祉_一人当たり賃金_都道府県_全産業.csv',encoding='shift_jis')
 
 st.header('■2019年：一人当たり平均賃金のヒートマップ')
 
